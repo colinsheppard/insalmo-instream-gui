5fb2f4c21c8c88674ed21f15d34de82cfc1670df
colinsheppard <colinthegaunt@gmail.com>
2011-12-16 15:55:29 -0800

    Debugging issues with metaproject data upon loading a sample project.

--- InSALMO/src/insalmo/MetaProject.java
+++ InSALMO/src/insalmo/MetaProject.java
@@ -118,7 +118,7 @@ public class MetaProject {
 	        } catch (Exception e) {//Catch exception if any
 	            System.err.println("Error: " + e.getMessage());
 	        }
-	        this.paramNameListByParamType.put(paramType, nameList);
+	        MetaProject.paramNameListByParamType.put(paramType, nameList);
 		}
 	}
 	
